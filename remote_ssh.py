909b952b0f20ebd63fdb0b65e96b9f6e6dca8a2b
vpalex999 <vpalex@mail.ru>
2023-03-11 10:16:26 +0500

    add some cuncurrency

--- remote_ssh.py
+++ remote_ssh.py
@@ -6,6 +6,7 @@ Options:
     <filename>      the configuration file (*.yaml)
 """
 import asyncio
+from typing import Any, Dict, List
 import asyncssh
 import sys
 from docopt import docopt
@@ -14,30 +15,27 @@ from src.config import Config
 from src.utils import sudo_convert
 
 
-async def run_client(config: Config) -> None:
-    nodes = config.nodes
-    commands = config.commands
-    if len(nodes) > 0:
-        for node in nodes:
-            url = node["url"]
-            username = node["username"]
-            password = node["password"]
+async def echo_node(node_config: Dict[str, Any], commands: List[str]) -> None:
+    url = node_config["url"]
+    username = node_config["username"]
+    password = node_config["password"]
 
-            for command in commands:
-                async with asyncssh.connect(
-                        url, password=password, username=username) as conn:
-                    result = await conn.run(sudo_convert(command, password), check=True)
+    async with asyncssh.connect(
+            url, password=password, username=username) as conn:
+        for command in commands:
+            result = await conn.run(sudo_convert(command, password), check=True)
 
-                    if result.exit_status == 0:
-                        print(f"node {url} command '{command}':\n{result.stdout}")
-                    else:
-                        print(f"node {url}: {result.stderr}",
-                              end='', file=sys.stderr)
-                        print(f"node {url}: Program exited with status {result.exit_status}",
-                              file=sys.stderr)
+            if result.exit_status == 0:
+                print(
+                    f"node {url} command '{command}':\n{result.stdout}")
+            else:
+                print(f"node {url}: {result.stderr}",
+                      end='', file=sys.stderr)
+                print(f"node {url}: Program exited with status {result.exit_status}",
+                      file=sys.stderr)
 
 
-if __name__ == "__main__":
+async def main() -> None:
     arguments = docopt(__doc__)  # type: ignore
     print(arguments)
 
@@ -45,9 +43,18 @@ if __name__ == "__main__":
         config = Config.from_file(arguments["<filename>"])
         print(str(config))
 
-        try:
-            asyncio.run(run_client(config))
-        except (OSError) as exc:
-            sys.exit('SSH connection failed: ' + str(exc))
-        except (asyncssh.Error) as err:
-            sys.exit('SSH command failed: ' + str(err))
+        echo_tasks: List[asyncio.Task] = []
+
+        for node in config.nodes:
+            echo_tasks.append(asyncio.create_task(
+                echo_node(node, config.commands)))
+
+        [await task for task in echo_tasks]
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (OSError) as exc:
+        sys.exit('SSH connection failed: ' + str(exc))
+    except (asyncssh.Error) as err:
+        sys.exit('SSH command failed: ' + str(err))
